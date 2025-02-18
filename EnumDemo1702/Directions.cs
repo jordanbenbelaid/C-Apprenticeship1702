64f4c62f1ca55bc7ef645f0aaa2b69c27aa085f1
Jordan <jordanbenbelaid@hotmail.com>
2025-02-18 12:02:23 +0000

    Added the enum demo and enum task solution

---
+++ EnumDemo1702/Directions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo1702
+{
+    public enum Directions
+    {
+        NORTH = 1 , EAST = 2, SOUTH = 3, WEST = 4
+    }
+}
---
+++ EnumDemo1702/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace EnumDemo1702
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            
+
+            Directions direction = Directions.NORTH;
+
+            switch (direction)
+            {
+                case Directions.NORTH:
+                    Console.WriteLine("You are travelling North");
+                    break;
+                case Directions.EAST:
+                    Console.WriteLine("You are travelling East");
+                    break;
+                case Directions.SOUTH:
+                    Console.WriteLine("You are travelling South");
+                    break;
+                case Directions.WEST:
+                    Console.WriteLine("You are travelling West");
+                    break;
+            }
+
+
+        }
+    }
+}
