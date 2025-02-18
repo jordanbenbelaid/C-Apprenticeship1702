07ec3ca00287589a7477eb7ad7a8fac8af31d120
Jordan <jordanbenbelaid@hotmail.com>
2025-02-18 16:16:45 +0000

    Completed the animal class

---
+++ Zoo/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public class Animal
+    {
+
+        //Fields/Attributes
+        private int Id;
+        private string Colour;
+        private string Species;
+        private bool HasFourLegs;
+
+        //Constructors
+        public Animal() { }
+        public Animal(int id, string colour, string species, bool hasFourLegs)
+        {
+            this.Id1 = id;
+            this.Colour1 = colour;
+            this.Species1 = species;
+            this.HasFourLegs1 = hasFourLegs;
+        }
+
+        //Getters and Setters
+        public int Id1 { get => Id; set => Id = value; }
+        public string Colour1 { get => Colour; set => Colour = value; }
+        public string Species1 { get => Species; set => Species = value; }
+        public bool HasFourLegs1 { get => HasFourLegs; set => HasFourLegs = value; }
+
+        //Methods/Behaviours of the Class
+        public void noise()
+        {
+            Console.WriteLine("Animal noise");
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return $"Animal: \nID: {Id1} \nColour: {Colour1} \n" +
+                $"Species: {Species1} \nDoes it have 4 legs?: {HasFourLegs1}";
+        }
+
+
+
+
+    }
+}
---
+++ Zoo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal Elephant = new Animal(1, "Grey", "African", true);
+
+            //get using generated getters
+            Console.WriteLine(Elephant.Colour1);
+
+            //set using generated setters
+            Elephant.Colour1 = "Blue";
+            Console.WriteLine(Elephant.Colour1);
+        }
+    }
+}
